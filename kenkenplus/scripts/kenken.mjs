d8d137ef636a9a72cf449e98de7ae67cd2faa5e3
Joseph Campbell <campbellbjoseph@gmail.com>
2023-12-17 17:59:21 -0500

    Add better win message

--- kenkenplus/scripts/kenken.mjs
+++ kenkenplus/scripts/kenken.mjs
@@ -364,6 +364,8 @@ function checkPuzzle() {
     }
     if (errors == 0) {
         document.getElementById("title").innerText = "You win!";
+        createBalloons(document.getElementById("title"), 30);
+          
     } else {
         console.log(errors);
         let submit = document.getElementById("submit");
@@ -450,3 +452,39 @@ function stopDeleting() {
     d.removeEventListener("click", stopDeleting);
     d.addEventListener("click", startDeleting);
 }
+
+function random(num) {
+    return Math.floor(Math.random() * num);
+  }
+  
+function getRandomStyles() {
+    var r = random(255);
+    var g = random(255);
+    var b = random(255);
+    var mt = random(200);
+    var ml = random(50);
+    var dur = random(5) + 5;
+    return `
+    background-color: rgba(${r},${g},${b},0.7);
+    color: rgba(${r},${g},${b},0.7); 
+    box-shadow: inset -7px -3px 10px rgba(${r - 10},${g - 10},${b - 10},0.7);
+    margin: ${mt}px 0 0 ${ml}px;
+    animation: float ${dur}s ease-in infinite
+    `;
+  }
+  
+function createBalloons(balloonContainer, num) {
+    for (var i = num; i > 0; i--) {
+      var balloon = document.createElement("div");
+      balloon.className = "balloon";
+      balloon.style.cssText = getRandomStyles();
+      balloonContainer.append(balloon);
+    }
+  }
+  
+function removeBalloons(balloonContainer) {
+    balloonContainer.style.opacity = 0;
+    setTimeout(() => {
+      balloonContainer.remove()
+    }, 500)
+  }
